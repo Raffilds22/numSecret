6466f34d9cde6dbd1335ffb1dde681b986b229d0
Raffilds22 <115758447+Raffilds22@users.noreply.github.com>
2025-03-04 19:17:23 -0300

    JOGO_DA_RAFA

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'JOGO DO NÚMERO SECRETO');
+    exibirTextoNaTela('h1', 'JOGO DO NÚMERO SECRETO DA RAFA');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
